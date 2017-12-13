c1b639ede5e615cd355b6d0a6a6186ee2c3fe45d
Gubbels <d.j.m.gubbels@student.tue.nl>
2017-12-13 13:22:43 +0100

    v2

---
+++ list/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.todo_list, name='todo_list'),
+    url(r'^todo/(?P<pk>\d+)/$', views.todo_detail, name='todo_detail'),
+    url(r'^todo/new/$', views.todo_edit, name='todo_edit'),
+]
--- list/views.py
+++ list/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
+def todo_list(request):
+    todos = Todo.objects.filter(
+        deadline_date__lte=timezone.now()).order_by('deadline_date')
+    return render(request, 'todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    return render(request, 'todo_detail.html', {'todo': todo})
+
+def todo_edit(request):
+    form = TodoForm()
+    return render(request, 'todo_edit.html', {'form': form})
